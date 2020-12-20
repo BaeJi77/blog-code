b956a9fd2cbc09f20ea5fddfc1e9d3dc682ea499
baeji.hoon <baeji.hoon@linecorp.com>
2020-12-21 01:32:07 +0900

    Update vegeta set-up file

--- 2020-12/vegeta/vegeta.sh
+++ 2020-12/vegeta/vegeta.sh
@@ -1,10 +1,19 @@
-vegeta attack -rate=1 -duration=5s -targets=targets.txt | vegeta report 
+# echo "GET http://127.0.0.1:9997/" | vegeta attack -duration=5s | vegeta report 
 
-vegeta attack -rate=1000 -duration=5s -targets=targets.txt | vegeta report -type='hist[0, 2ms, 4ms, 6ms, 8ms, 10ms]'
+# echo "GET http://127.0.0.1:9997/" | vegeta attack -duration=5s | vegeta report -type=json > result.json
 
-vegeta attack -rate=10 -duration=5s -targets=targets.txt | vegeta report -type=json > result.json
+# echo "GET http://127.0.0.1:9997/" | vegeta attack -duration=5s | vegeta report -type='hist[0, 2ms, 4ms, 6ms, 8ms, 10ms]'
 
-vegeta attack -name=50qps -rate=50 -duration=5s -targets=targets.txt > results.50qps.bin
-cat results.50qps.bin | vegeta plot > plot.50qps.html
+# multi host
+# vegeta attack -rate=1 -duration=5s -targets=targets.txt | vegeta report 
 
+# vegeta attack -rate=1000 -duration=5s -targets=targets.txt | vegeta report -type='hist[0, 2ms, 4ms, 6ms, 8ms, 10ms]'
+
+# vegeta attack -rate=10 -duration=5s -targets=targets.txt | vegeta report -type=json > result.json
+
+# plot
+# vegeta attack -name=50qps -rate=50 -duration=5s -targets=targets_2.txt > results.50qps.bin
+# cat results.50qps.bin | vegeta plot > plot.50qps.html
+
+# rate=0
 # vegeta attack -rate=0 -max-workers=5 -duration=5s -targets=targets.txt | vegeta report 
